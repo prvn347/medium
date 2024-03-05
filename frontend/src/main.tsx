6a1295a5628566181cfc7b81a9f799be155d70e2
Your New Username <sahupravin960@gmail.com>
2024-03-05 16:18:24 +0530

    final build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <GoogleOAuthProvider clientId="907037876330-i9k8iuc3uqm985mj8321i4o05u7ni5lc.apps.googleusercontent.com">
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </GoogleOAuthProvider>
+  </React.StrictMode>,
 )
