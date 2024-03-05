ba5b69b5ff98fbda62fc5eb4b1448e6cacaba243
Your New Username <sahupravin960@gmail.com>
2024-03-05 14:32:34 +0530

    adding google auth

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId="907037876330-i9k8iuc3uqm985mj8321i4o05u7ni5lc.apps.googleusercontent.com">
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </GoogleOAuthProvider>
 )
